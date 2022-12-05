1c49de9150200480606c0238afa9f322a26ae05f
Pau Civill <pau.civill@gmail.com>
2022-12-05 10:19:53 +0100

    userSlice

--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -2,13 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
     name: 'userData',
-    initialState: JSON.parse(sessionStorage.getItem('user')) ?? { username: 'Pepe' },
+    initialState: JSON.parse(sessionStorage.getItem('user')) ?? { username: '', isLogged: false },
     reducers: {
-        LOG_USER: (state, action) => {
-            
+        LOG_IN: (state, action) => {
+            const loggedUser = {
+                ...action.payload,
+                isLogged: true
+            }
+            sessionStorage.setItem('user', JSON.stringify(loggedUser));
+            state = loggedUser;
         },
         LOG_OUT: (state) => {
-
+            sessionStorage.removeItem('user');
+            state = { username: '', isLogged: false }
         },
     }
 })
