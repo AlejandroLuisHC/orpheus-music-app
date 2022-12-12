7e1fe1e4f943ec716bf9d5ecc042a1b340787f80
Pau Civill <pau.civill@gmail.com>
2022-12-12 14:08:56 +0100

    feat(search): first approach

---
+++ src/components/style/searchStyle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainFlexContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
+
+export const DivSearchResults = styled.div`
+    flex-grow: 1;
+    overflow-y: auto;
+    padding: 1rem;
+`;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import fetchKey from '../api/fetchKey';
+import { FormStyle, InputStyle } from '../components/style/generalStyle';
+import {
+  DivSearchResults,
+  MainFlexContainer,
+} from '../components/style/searchStyle';
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+  const { data: playlists } = useQuery(['playlists', 'playlists'], () =>
+    fetchKey('playlists')
+  );
 
-export default Search
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const [searchResults, setSearchResults] = useState([]);
+  console.log('searchResults', searchResults);
+
+  const search = watch('search', '').toLowerCase();
+  console.log(search)
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    fetchKey('playlists');
+    // let cancel = false;
+    // if (cancel) return;
+
+    setSearchResults(playlists.map((playlist) => {
+      if (playlist.name.toLowerCase().includes(search) || playlist.description.toLowerCase().includes(search)) {
+        return {
+          name: playlist.name,
+          img: playlist.img
+        };
+      }
+    }));
+
+    // return () => cancel = true;
+  }, [search]);
+
+  return (
+    <MainFlexContainer>
+      <FormStyle>
+        <InputStyle
+          type="search"
+          placeholder="Users, songs, playlists, events, genres or moods"
+          {...register('search')}
+        ></InputStyle>
+      </FormStyle>
+
+      <DivSearchResults>
+        {search === 'pop' && 'pop related results'}
+        {searchResults?.map((result) => result?.name)}
+      </DivSearchResults>
+    </MainFlexContainer>
+  );
+};
+
+export default Search;
