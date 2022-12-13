2fdcb0d45731400befa589904e4c225de24d694d
Jose-Morcillo-Cascales <morcillopepe1@gmail.com>
2022-12-13 15:19:03 +0100

    Create music player component

---
+++ src/components/music_player/MusicPlayer.jsx
@@ -0,0 +1,33 @@
+import { Player } from "react-simple-player"
+import { ButtonClosePlayer, DivInfoTrack, DivMusicPlayer, DivMusicPlayerInfo, SpanPlayerName, SpanPlayerUser } from "../style/generalStyle"
+import { IoMdClose } from "react-icons/io"
+
+const MusicPlayer = ({audio,name,user,setPlayer}) => {
+  return (
+
+    <DivMusicPlayer>
+        <Player 
+            src={audio} 
+            height={40} 
+            grey={[18, 18, 18]} 
+            accent={[239, 184, 16]} 
+            // autoPlay={true}
+        />
+        <DivMusicPlayerInfo>
+            <DivInfoTrack>
+                <SpanPlayerName>{name}</SpanPlayerName>
+                <SpanPlayerUser>{user}</SpanPlayerUser>
+            </DivInfoTrack>
+            <ButtonClosePlayer onClick={() => setPlayer(prev => prev = {
+                ...prev,
+                playerOn: false
+            })}>
+                <IoMdClose />
+            </ButtonClosePlayer>
+        </DivMusicPlayerInfo>
+    </DivMusicPlayer>
+  )
+}
+
+export default MusicPlayer
+
