b65a07618161af2f2244e276e30a1314a8f1a637
BalaguerDev <101297683+BalaguerDev@users.noreply.github.com>
2022-12-14 13:42:26 +0100

    fix: data.json, imgs

--- src/assets/img/gallery_slides/index.js
+++ src/assets/img/gallery_slides/index.js
@@ -1,9 +1,11 @@
-import slide0 from './slide0.jpg'
 import slide1 from './slide1.jpg'
-import slide2 from  './slide2.jpg'
+import slide2 from './slide2.jpg'
+import slide3 from  './slide3.jpg'
+import slide4 from  './slide4.jpg'
 
 export {
-    slide0,
     slide1,
-    slide2
+    slide2,
+    slide3,
+    slide4
 }
