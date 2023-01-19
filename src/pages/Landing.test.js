c1f68b58e39e7625a023ea485f51106dbf2c2acd
AlejandroLHC <alejandrolhc@gmail.com>
2023-01-19 17:53:57 +0100

    feat: Landing test / tabicon and tabtitle

---
+++ src/pages/Landing.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import Landing from "./Landing";
+
+describe("Landing Page", () => {
+    let width;
+    let setWidth;
+    beforeEach(() => {
+        width = jest.fn();
+        setWidth = jest.fn();
+        useWidth.mockImplementation(() => [width, setWidth]);
+    });
+    
+    test("should display the logo, slogan and sign-up button when the screen width is less than 768px", () => {
+        // Arrange
+        width.mockReturnValue(767);
+        const { getByAltText, getByText } = render(<Landing />);
+        // Act
+        const logo = getByAltText("Logo");
+        const slogan = getByText("The music that unites us");
+        const signUpButton = getByText("Sign Up");
+        // Assert
+        expect(logo).toBeInTheDocument();
+        expect(slogan).toBeInTheDocument();
+        expect(signUpButton).toBeInTheDocument();
+    });
+
+    test("should display the logo, slogan, sign-up button and log-in button when the screen width is more than 768px", () => {
+        // Arrange
+        width.mockReturnValue(769);
+        const { getByAltText, getByText } = render(<Landing />);
+        // Act
+        const logo = getByAltText("Logo");
+        const slogan = getByText("The music that unites us");
+        const signUpButton = getByText("Sign Up");
+        // Assert
+        expect(logo).toBeInTheDocument();
+        expect(slogan).toBeInTheDocument();
+        expect(signUpButton).toBeInTheDocument();
+    });
+});
