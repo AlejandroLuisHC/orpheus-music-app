cea35ff26f7ed1decfb26e9b4945fa5c4a7694f0
AlejandroLHC <alejandrolhc@gmail.com>
2023-02-01 12:06:54 +0100

    fix: Aside playlist last 10 reverse order

--- src/components/general_components/aside/AsideRecentPlaylist.jsx
+++ src/components/general_components/aside/AsideRecentPlaylist.jsx
@@ -9,7 +9,7 @@ const AsideRecentPlaylist = () => {
     return (
         <>
             <H5StyleAside onClick={() => navigate('/home/playlists')}>Last playlists</H5StyleAside>
-            {playlists?.slice(0, 9).map(playlist =>
+            {playlists?.slice(-10).reverse().map(playlist =>
                 <LinkAside to={`/playlist/${playlist._id}`} key={playlist._id}>
                     <p>{playlist.name}</p>
                 </LinkAside>
